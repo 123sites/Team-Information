6244b310f5153260d36246bf2663604a21c5a976
CF <cfreitas12345@gmail.com>
2023-03-02 10:44:45 -0700

    Added assignment instructions.

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,4 +1,6 @@
-// The application must have these classes: Employee, Manager, Engineer, and Intern. The tests for these classes (in the _tests_ directory) must all pass.
+// The application must have these classes: Employee, Manager, Engineer, and Intern. 
+// The tests for these classes (in the _tests_ directory) must all pass.
+// The other three classes will extend Employee.
 // The first class is an Employee parent class with the following properties and methods:
 
 // name
