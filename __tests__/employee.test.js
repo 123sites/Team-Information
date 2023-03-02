ce4cb70f0058bf4b59f5a423b60191cba5d66c64
CF <cfreitas12345@gmail.com>
2023-03-02 10:35:18 -0700

    Coded the employee.test.js

---
+++ __tests__/employee.test.js
@@ -0,0 +1,36 @@
+// The application must have these classes: Employee, Manager, Engineer, and Intern. The tests for these classes (in the _tests_ directory) must all pass.
+// The first class is an Employee parent class with the following properties and methods:
+
+// name
+// id
+// email
+// getName()
+// getId()
+// getEmail()
+// getRole() Returns 'Employee'
+
+module.exports = class Employee {
+
+  constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+  }
+
+  getName() {
+      return this.name;
+  }
+
+  getId() {
+      return this.id;
+  }
+
+  getEmail() {
+      return this.email;
+  }
+
+  getRole() {
+      return "Employee";
+  }
+
+}
